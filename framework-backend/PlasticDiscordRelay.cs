6ea04b36fa58414991da2eafdf35aedab8b966d7
darketomaly <darketomaly@gmail.com>
2026-06-27 08:07:27 -0700

    Jira relay

--- framework-backend/PlasticDiscordRelay.cs
+++ framework-backend/PlasticDiscordRelay.cs
@@ -5,17 +5,38 @@ namespace framework_backend;
 
 public static class PlasticDiscordRelay
 {
-    private static readonly ulong TargetChannelId = 1517147424982958281; 
+    private const ulong ChannelIdPlastic = 1517147424982958281;
+    private const ulong ChannelIdJira = 1518976935277891594;
 
     public static void Configure(WebApplication app, DiscordSocketClient client)
     {
+        // Plastic
+        
         app.MapPost("/plastic-discord-webhook", async (HttpContext context) =>
         {
             using var reader = new StreamReader(context.Request.Body);
             string body = await reader.ReadToEndAsync();
 
             // Send message using the bot
-            var channel = await client.GetChannelAsync(TargetChannelId) as IMessageChannel;
+            var channel = await client.GetChannelAsync(ChannelIdPlastic) as IMessageChannel;
+
+            if (channel != null)
+            {
+                await channel.SendMessageAsync(body);
+            }
+
+            return Results.Ok();
+        });
+        
+        // Jira
+        
+        app.MapPost("/jira-discord-webhook", async (HttpContext context) =>
+        {
+            using var reader = new StreamReader(context.Request.Body);
+            string body = await reader.ReadToEndAsync();
+
+            // Send message using the bot
+            var channel = await client.GetChannelAsync(ChannelIdJira) as IMessageChannel;
 
             if (channel != null)
             {
