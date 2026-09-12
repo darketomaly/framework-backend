5791b94f5a680abbafceb1dec999e0e337418187
darketomaly <darketomaly@gmail.com>
2026-09-12 17:43:37 +0800

    Removed discord relay explanation comment, as it's now on the readme file

--- src/Integrations/DiscordRelay.cs
+++ src/Integrations/DiscordRelay.cs
@@ -3,36 +3,6 @@ using Discord;
 using Discord.WebSocket;
 namespace framework_backend;
 
-/*
- 
- Using Jira relay
- 
- All automations have these, nothing per-automation specific:
- 1. Automation web request url: https://apiurl/jira-discord-webhook?channel=desiredchannelid
- 2. Custom data:
- {
-    "event": "{{rule.name}}",
-    "initiator_display_name" : "{{initiator.displayName}}",
-    "issue_key" : "{{issue.key}}",
-    "issue_name" : "{{issue.summary.jsonEncode}}",
-    "initiator_icon" : "{{initiator.avatarUrls."48x48"}}",
-    "version_released" : "{{version.name}}",
-    "sprint_name" : "{{sprint.name}}",
-    "rejection_reason" : "{{issue.customfield_10044.jsonEncode()}}"
- }
- 
- And the automation names should be:
- discord-issue-approved
- discord-issue-created
- discord-issue-ready-for-review
- discord-issue-rejected
- discord-issue-revision
- discord-issue-start
- discord-sprint-completed
- discord-sprint-start
- discord-version-released
- */
-
 public static class DiscordRelay
 {
     public static void Configure(WebApplication app, DiscordSocketClient client)
